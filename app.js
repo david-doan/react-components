b8c45f16660f060b19bca8960b6dd220644177c2
david-doan <a.david.doan@gmail.com>
2016-05-06 11:36:31 -0700

    GroceryListItems created via passing props

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // TODO
+var GroceryList =  () => (
+  <div>
+    <h1>Grocery List</h1>
+    <GroceryListItem groceryItems={['Apples','Oranges']}/>
+  </div> 
+);
+
+var GroceryListItem = (props) => (
+  <ul>
+    <li>{props.groceryItems[0]}</li>
+    <li>{props.groceryItems[1]}</li>
+    <li>{props.groceryItems[2]}</li>
+  </ul>
+);
+
+// var Apples = () => (
+//   <li>Apples are red</li>
+// );
+
+// var Oranges = () => (
+//   <li>Oranges are orange</li>
+// );
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
